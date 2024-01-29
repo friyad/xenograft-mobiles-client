18672211255d2bc9a9097adbacfd06de9cced5c8
FriyadDev <riyadhasanblue2323@gmail.com>
2024-01-30 05:08:15 +0600

    chore(global): :lipstick: SCSS installed and configured in all files

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import { RouterProvider } from "react-router-dom";
 import { routes } from "@/routes/routes";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import "./index.css";
+import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={routes} />
+      <main>
+        <RouterProvider router={routes} />
+      </main>
     </Provider>
   </React.StrictMode>
 );
