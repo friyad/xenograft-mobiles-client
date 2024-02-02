ee4f0979e960fe73006d7a4efb5dffb2f493e985
FriyadDev <riyadhasanblue2323@gmail.com>
2024-02-02 16:59:07 +0600

    refactor(global): :lipstick: Few styles changed on Button and Dropdown component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,9 +3,10 @@ import { Slot } from "@radix-ui/react-slot";
 import { cva, VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
+import { Loader } from "lucide-react";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded lg:rounded-md text-2xs sm:text-xs 2xl:text-sm 3xl:text-base font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50  transition-transform duration-200 active:scale-[0.98] shadow-sm hover:shadow-lg transition-all duration-200 select-none",
+  "inline-flex items-center justify-center whitespace-nowrap rounded lg:rounded-md text-2xs sm:text-xs 2xl:text-sm 3xl:text-base font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50  transition-transform duration-200 active:scale-[0.98] shadow-sm hover:shadow-lg transition-all duration-200 select-none relative",
   {
     variants: {
       variant: {
@@ -39,17 +40,35 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
+  loading?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button";
+  (
+    {
+      className,
+      variant,
+      size,
+      children,
+      asChild = false,
+      loading = false,
+      ...props
+    },
+    ref
+  ) => {
     return (
-      <Comp
+      <button
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
-      />
+      >
+        {children}
+        {loading && (
+          <div className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 flex justify-center items-center w-fit h-fit z-20">
+            <Loader className="animate-spin" />
+          </div>
+        )}
+      </button>
     );
   }
 );
