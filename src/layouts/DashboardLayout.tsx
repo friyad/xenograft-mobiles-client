4875143320771e0379b4d927bcfef510a622bee1
FriyadDev <riyadhasanblue2323@gmail.com>
2024-02-02 16:58:21 +0600

    style(dashboard): :construction: Dashboard Layout protected with Privet Route and few other style changes

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -6,6 +6,7 @@ import {
 } from "@/components/ui/resizable";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
+import PrivetRoute from "@/components/PrivetRoute";
 
 interface DashboardProps {
   children: ReactNode;
@@ -14,33 +15,35 @@ interface DashboardProps {
 
 const DashboardLayout = ({ title, children }: DashboardProps) => {
   return (
-    <ResizablePanelGroup
-      // autoSaveId="persistence"
-      direction="horizontal"
-      className="bg-cusGray-300 min-h-screen overflow-auto md:overflow-hidden"
-    >
-      <ResizablePanel
-        defaultSize={14}
-        minSize={0}
-        maxSize={18}
-        className="min-w-[180px] xl:min-w-max hidden md:inline-block"
+    <PrivetRoute>
+      <ResizablePanelGroup
+        // autoSaveId="persistence"
+        direction="horizontal"
+        className="bg-cusGray-300 min-h-screen overflow-auto md:overflow-hidden"
       >
-        <div className="bg-cusGray-200 h-full rounded-r-xl xl:rounded-r-3xl overflow-hidden ">
-          <Sidebar />
-        </div>
-      </ResizablePanel>
-
-      <ResizableHandle withHandle={true} className="hidden md:flex" />
-
-      <ResizablePanel defaultSize={82} className="">
-        <div className="h-full">
-          <Navbar title={title} />
-          <div className="mt-20 md:mt-5 bg-cusGray-100 md:h-[calc(100vh-84px)] mxl:h-[calc(100vh-100px)] rounded-t-xl xl:rounded-t-3xl shadow-[0px_34px_64px_0px_#b1c8e82e] md:overflow-auto">
-            {children}
+        <ResizablePanel
+          defaultSize={14}
+          minSize={0}
+          maxSize={18}
+          className="min-w-[180px] xl:min-w-[0px] hidden md:inline-block"
+        >
+          <div className="bg-cusGray-200 h-full rounded-r-xl xl:rounded-r-3xl overflow-hidden ">
+            <Sidebar />
           </div>
-        </div>
-      </ResizablePanel>
-    </ResizablePanelGroup>
+        </ResizablePanel>
+
+        <ResizableHandle withHandle={true} className="hidden md:flex" />
+
+        <ResizablePanel defaultSize={82} className="">
+          <div className="h-full">
+            <Navbar title={title} />
+            <div className="mt-20 md:mt-5 bg-cusGray-100 md:h-[calc(100vh-84px)] mxl:h-[calc(100vh-100px)] rounded-t-xl xl:rounded-t-3xl shadow-[0px_34px_64px_0px_#b1c8e82e] md:overflow-auto">
+              {children}
+            </div>
+          </div>
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </PrivetRoute>
   );
 };
 
